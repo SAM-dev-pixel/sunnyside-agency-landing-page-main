67e5e8f457aea477c9afa14ed3baba46052f900a
SAM <75918417+SAM-dev-pixel@users.noreply.github.com>
2022-03-14 17:31:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ const navbar = document.querySelector('.navbar');
   burger.addEventListener('click', ()=> document.querySelector('.nav-links').classList.toggle('show'));
   
 })(document.querySelector('.burger-btn'));
+
+// animations
+const loadIntro =()=> {
+
+  const headerTitle = document.querySelector('.header-title');
+
+  document.querySelectorAll('.intro-txt span').forEach(txt => txt.style.transform = 'translateX(0)');
+
+  document.querySelector('.red-slider').style.transform = 'translateX(100%)';
+
+  document.querySelector('.intro-txt-contain').style.transform = 'translateX(100%)';
+
+  headerTitle.style.opacity = 1;
+
+  headerTitle.style.transform = 'translateY(0)';
+
+} 
